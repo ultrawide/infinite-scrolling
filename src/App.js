51e7cbe40e71f5c9d45259bb7ee57cfb14f48e16
ultrawide <colinchan02@gmail.com>
2021-09-13 22:47:05 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ScrollComponent from './ScrollComponent'
+
+function App() {
+  return (
+    <div className="App">
+        <ScrollComponent/>
+    </div>
+  );
+}
+
+export default App;
